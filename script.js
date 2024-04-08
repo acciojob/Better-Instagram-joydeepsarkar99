4352561b9abf1714dd900fdde19346a27cd798c2
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-04-08 13:20:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-//your code here
+let images = document.querySelectorAll(".image")
+
+for (let i of images) {
+	i.addEventListener("dragstart", (eventDetails) => {
+		let draggedImage = eventDetails.target
+		eventDetails.dataTransfer.setData("text/plain", draggedImage.id)
+	})
+}
+
+let dragEvents = ["dragover", "dragenter", "drop"]
+dragEvents.forEach(dropEvents => {
+	let imageAll = document.querySelectorAll(".image")
+	for (let i of imageAll) {
+		i.addEventListener(dropEvents, (eventDetails) => {
+			eventDetails.preventDefault()
+
+			if (dropEvents == "drop") {
+				let draggedImageId = eventDetails.dataTransfer.getData("text/plain")
+				let draggedImage = document.querySelector(`#${draggedImageId}`)
+
+				let swappedImageId = eventDetails.target.id
+				let swappedImage = document.querySelector(`#${swappedImageId}`)
+
+				let prevDrag = draggedImage.previousElementSibling
+				let nextDrag = draggedImage.nextElementSibling
+
+				let prevSwap = swappedImage.previousElementSibling
+				let nextSwap = swappedImage.nextElementSibling
+
+				if (draggedImage === prevSwap) {
+					nextSwap.before(draggedImage)
+				}
+				else {
+
+					if (prevDrag != null) {
+						prevDrag.after(swappedImage)
+					}
+					else {
+						nextDrag.before(swappedImage)
+					}
+
+					if (prevSwap != null) {
+						prevSwap.after(draggedImage)
+					}
+					else {
+						nextSwap.before(draggedImage)
+					}
+				}
+
+
+
+			}
+		})
+	}
+})

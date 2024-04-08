cbe9f22e3b87bcad3d43944c15cc9adc98fd475c
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-04-08 13:57:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,55 +1,35 @@
-let imagesDiv = document.querySelectorAll(".image")
-imagesDiv.forEach((div) => {
-	div.addEventListener("dragstart", dragstartEvent)
-	div.addEventListener("dragover", dragoverEvent)
-	div.addEventListener("drop", dropEvent)
+const imageDivs = document.querySelectorAll(".image");
+imageDivs.forEach((div) => {
+  div.addEventListener("dragstart", handleDragStart);
+  div.addEventListener("dragover", handleDragOver);
+  div.addEventListener("drop", handleDrop);
+  div.setAttribute("draggable", true);
 })
 
 
-function dragstartEvent(eventDetails) {
-	let draggedImage = eventDetails.target
-	eventDetails.dataTransfer.setData("text/plain", draggedImage.id)
+function handleDragStart(event) {
+  event.dataTransfer.setData("text/plain", event.target.id);
 }
 
-function dragoverEvent(eventDetails) {
-	eventDetails.preventDefault()
+function handleDragOver(event) {
+  event.preventDefault();
 }
 
-function dropEvent(eventDetails) {
-	eventDetails.preventDefault()
-	let draggedImageId = eventDetails.dataTransfer.getData("text/plain")
-	let draggedImage = document.querySelector(`#${draggedImageId}`)
+function handleDrop(event) {
+  event.preventDefault();
+  const draggedId = event.dataTransfer.getData("text/plain");
+  const droppedId = event.target.id;
 
-	let swappedImageId = eventDetails.target.id
-	let swappedImage = document.querySelector(`#${swappedImageId}`)
+  const draggedElement = document.getElementById(draggedId);
+  const droppedElement = document.getElementById(droppedId);
 
-	let prevDrag = draggedImage.previousElementSibling
-	let nextDrag = draggedImage.nextElementSibling
 
-	let prevSwap = swappedImage.previousElementSibling
-	let nextSwap = swappedImage.nextElementSibling
+  if (draggedElement && droppedElement) {
+    const tempBgImage = draggedElement.style.backgroundImage;
+    draggedElement.style.backgroundImage = droppedElement.style.backgroundImage;
+    droppedElement.style.backgroundImage = tempBgImage;
 
-	if (draggedImage === prevSwap) {
-		swappedImage.after(draggedImage)
-	}
-	else if (draggedImage === nextSwap) {
-		swappedImage.before(draggedImage)
-	}
-	else {
-
-		if (prevDrag != null) {
-			prevDrag.after(swappedImage)
-		}
-		else {
-			nextDrag.before(swappedImage)
-		}
-
-		if (prevSwap != null) {
-			prevSwap.after(draggedImage)
-		}
-		else {
-			nextSwap.before(draggedImage)
-		}
-	}
-
+    draggedElement.id = droppedId;
+    droppedElement.id = draggedId;
+  }
 }

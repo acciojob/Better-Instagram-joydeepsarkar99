1f57a71a9a7be67502529c20ab21292d24dd5cc7
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-04-08 13:51:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
-let images = document.querySelectorAll(".image")
+let imagesDiv = document.querySelectorAll(".image")
+imagesDiv.forEach((div) => {
+	div.addEventListener("dragstart", dragstartEvent)
+	div.addEventListener("dragover", dragoverEvent)
+	div.addEventListener("drop", dropEvent)
+})
 
-for (let i of images) {
-	i.addEventListener("dragstart", (eventDetails) => {
-		let draggedImage = eventDetails.target
-		eventDetails.dataTransfer.setData("text/plain", draggedImage.id)
-	})
+
+function dragstartEvent(eventDetails) {
+	let draggedImage = eventDetails.target
+	eventDetails.dataTransfer.setData("text/plain", draggedImage.id)
 }
 
-let dragEvents = ["dragover", "dragenter", "drop"]
-dragEvents.forEach(dropEvents => {
-	let imageAll = document.querySelectorAll(".image")
-	for (let i of imageAll) {
-		i.addEventListener(dropEvents, (eventDetails) => {
-			eventDetails.preventDefault()
+function dragoverEvent(eventDetails) {
+	eventDetails.preventDefault()
+}
 
-			if (dropEvents == "drop") {
-				let draggedImageId = eventDetails.dataTransfer.getData("text/plain")
-				let draggedImage = document.querySelector(`#${draggedImageId}`)
+function dropEvent(eventDetails) {
+	eventDetails.preventDefault()
+	let draggedImageId = eventDetails.dataTransfer.getData("text/plain")
+	let draggedImage = document.querySelector(`#${draggedImageId}`)
 
-				let swappedImageId = eventDetails.target.id
-				let swappedImage = document.querySelector(`#${swappedImageId}`)
+	let swappedImageId = eventDetails.target.id
+	let swappedImage = document.querySelector(`#${swappedImageId}`)
 
-				let prevDrag = draggedImage.previousElementSibling
-				let nextDrag = draggedImage.nextElementSibling
+	let prevDrag = draggedImage.previousElementSibling
+	let nextDrag = draggedImage.nextElementSibling
 
-				let prevSwap = swappedImage.previousElementSibling
-				let nextSwap = swappedImage.nextElementSibling
+	let prevSwap = swappedImage.previousElementSibling
+	let nextSwap = swappedImage.nextElementSibling
 
-				if (draggedImage === prevSwap) {
-					nextSwap.before(draggedImage)
-				}
-				else {
-
-					if (prevDrag != null) {
-						prevDrag.after(swappedImage)
-					}
-					else {
-						nextDrag.before(swappedImage)
-					}
-
-					if (prevSwap != null) {
-						prevSwap.after(draggedImage)
-					}
-					else {
-						nextSwap.before(draggedImage)
-					}
-				}
-
-
-
-			}
-		})
+	if (draggedImage === prevSwap) {
+		swappedImage.after(draggedImage)
 	}
-})
+	else if (draggedImage === nextSwap) {
+		swappedImage.before(draggedImage)
+	}
+	else {
+
+		if (prevDrag != null) {
+			prevDrag.after(swappedImage)
+		}
+		else {
+			nextDrag.before(swappedImage)
+		}
+
+		if (prevSwap != null) {
+			prevSwap.after(draggedImage)
+		}
+		else {
+			nextSwap.before(draggedImage)
+		}
+	}
+
+}
